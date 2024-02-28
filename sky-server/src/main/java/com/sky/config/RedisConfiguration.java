b691c039707374a4938e59a85ad9e676412913e2
shyJyt <2873704761@qq.com>
2024-02-28 14:14:22 +0800

    test redis/httpclient/websocket

--- sky-server/src/main/java/com/sky/config/RedisConfiguration.java
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -1,6 +1,7 @@
 package com.sky.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -10,12 +11,13 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Slf4j
 public class RedisConfiguration {
 
+    @Bean
     public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         log.info("redisTemplate init...");
         RedisTemplate redisTemplate = new RedisTemplate();
         // 设置连接工厂
         redisTemplate.setConnectionFactory(redisConnectionFactory);
-        // key序列化
+        // key序列化，解决乱码的问题
         redisTemplate.setKeySerializer(new StringRedisSerializer());
 
         return redisTemplate;
